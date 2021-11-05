9abac8cbc34f61365450f9b8560974e254ebaf82
Uday Pandey <uday.pandey@gmail.com>
2021-11-05 10:41:00 +0000

    Added sample project

---
+++ MatchSigning/MatchSigningApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchSigningApp.swift
+//  MatchSigning
+//
+//  Created by Uday Pandey on 05/11/2021.
+//
+
+import SwiftUI
+
+@main
+struct MatchSigningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
